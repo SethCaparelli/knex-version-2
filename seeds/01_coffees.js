902659fcf4065ac9dc3500651c662e0726c11bb7
Seth <sethcaparelli@gmail.com>
2018-03-02 14:44:57 -0700

    tests pass

---
+++ seeds/01_coffees.js
@@ -0,0 +1,23 @@
+
+exports.seed = function(knex, Promise) {
+  return knex.raw("DELETE FROM coffee; ALTER SEQUENCE coffee_id_seq RESTART WITH 4;")
+    .then(function() {
+      const types = [{
+        id: 1,
+        name: "Black and Tan",
+        roaster: "Ink",
+        aroma: "3"
+      }, {
+        id: 2,
+        name: "Holiday Roast",
+        roaster: "Starbucks",
+        aroma: "9"
+      }, {
+        id: 3,
+        name: "House Quake",
+        roaster: "Denver Coffee",
+        aroma: "6"
+      }]
+      return knex("coffee").insert(types)
+    })
+};
